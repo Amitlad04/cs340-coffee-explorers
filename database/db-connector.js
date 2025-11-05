63c28fd12d160b281b273028c278b9b385c47c68
Ncheemu <viggolarsen@outlook.com>
2025-11-04 23:06:59 -0800

    Removed some logging statements

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,14 +1,6 @@
 // Get an instance of mysql we can use in the app
 let mysql = require('mysql2')
 
-
-console.log('DB Config:', {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW ? '***hidden***' : undefined,
-    database: process.env.DB
-});
-
 // Create a 'connection pool' using the provided credentials
 const pool = mysql.createPool({
     waitForConnections: true,
