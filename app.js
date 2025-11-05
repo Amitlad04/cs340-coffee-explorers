9263664abe66bfcb82d664e206085434e6b08219
Ncheemu <viggolarsen@outlook.com>
2025-11-04 22:33:22 -0800

    All get routes defined

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
+
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const PORT = 3456;
+const PORT = process.env.PORT || 3457;
 
 // Database
 const db = require('./database/db-connector');
 
 // Handlebars
 const { engine } = require('express-handlebars'); // Import express-handlebars engine
+
 app.engine('.hbs', engine({ extname: '.hbs' })); // Create instance of handlebars
 app.set('view engine', '.hbs'); // Use handlebars engine for *.hbs files.
 
@@ -28,9 +33,113 @@ app.get('/', async function (req, res) {
     }
 });
 
+// COFFEES TABLE
+app.get('/coffees', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // In query1, we use a JOIN clause to display the names of the homeworlds
+        const query1 = `SELECT * FROM Coffees;`;
+
+        const [coffees] = await db.query(query1);
+
+        // Render the coffees.hbs file, and also send the renderer
+        //  an object that contains our coffees information
+        res.render('coffees', { coffees: coffees });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/customers', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // In query1, we use a JOIN clause to display the names of the homeworlds
+        const query1 = `SELECT * FROM Customers;`;
+
+        const [customers] = await db.query(query1);
+
+        // Render the coffees.hbs file, and also send the renderer
+        //  an object that contains our coffees information
+        res.render('customers', { customers: customers });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/orders', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // In query1, we use a JOIN clause to display the names of the homeworlds
+        const query1 = `SELECT * FROM Orders;`;
+
+        const [orders] = await db.query(query1);
+
+        // Render the coffees.hbs file, and also send the renderer
+        //  an object that contains our coffees information
+        res.render('orders', { orders: orders });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/orders_has_coffees', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // In query1, we use a JOIN clause to display the names of the homeworlds
+        const query1 = `SELECT * FROM Orders_has_Coffees;`;
+
+        const [orders_has_coffees] = await db.query(query1);
+
+        // Render the coffees.hbs file, and also send the renderer
+        //  an object that contains our coffees information
+        res.render('orders_has_coffees', { orders_has_coffees: orders_has_coffees });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.get('/paymentMethods', async function (req, res) {
+    try {
+        // Create and execute our queries
+        // In query1, we use a JOIN clause to display the names of the homeworlds
+        const query1 = `SELECT * FROM PaymentMethods;`;
+
+        const [paymentMethods] = await db.query(query1);
+
+        // Render the coffees.hbs file, and also send the renderer
+        //  an object that contains our coffees information
+        res.render('paymentMethods', { paymentMethods: paymentMethods });
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+
 // ########################################
 // ########## LISTENER
 
 app.listen(PORT, function () {
     console.log('Express started on http://classwork.engr.oregonstate.edu:' + PORT + '; press Ctrl-C to terminate.');
 });
+
+
--- database/db-connector.js
+++ database/db-connector.js
@@ -1,14 +1,22 @@
 // Get an instance of mysql we can use in the app
 let mysql = require('mysql2')
 
+
+console.log('DB Config:', {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW ? '***hidden***' : undefined,
+    database: process.env.DB
+});
+
 // Create a 'connection pool' using the provided credentials
 const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
-    host              : 'classmysql.engr.oregonstate.edu',
-    user              : 'cs340_lada',
-    password          : 'v975OIYkCUo6',
-    database          : 'cs340_lada'
+    host              : process.env.DB_HOST,
+    user              : process.env.DB_USER,
+    password          : process.env.DB_PW,
+    database          : process.env.DB
 }).promise(); // This makes it so we can use async / await rather than callbacks
 
 // Export it for use in our application
